8636f213850cf90aa01bbbd2f36d82c4ef86968b
deng47 <deng47@gmail.com>
2016-12-23 19:14:09 -0800

    code optimization

---
+++ poollog.py
@@ -0,0 +1,24 @@
+def readpool(pool, tag):
+    pool=[]
+    try:
+        file = open('/home/pi/test/seiyuu2.0/record/'+tag[0:-1]+'.txt','r', encoding='utf-8')
+        pool = eval(file.read())
+        file.close()
+        print('读取'+tag+'更新记录')
+    except:
+        pass
+    return pool   
+
+def log(record):
+    try:
+        file = open('/home/pi/test/seiyuu2.0/record/'+'log.txt','a', encoding='utf-8')
+        file.write(str(record)+'\n')
+        file.close()
+    except:
+        pass
+        
+def update(pool, tag, limit):
+    if len(pool)>limit*2:
+        pool=pool[-limit*2:]  
+    file = open('/home/pi/test/seiyuu2.0/record/'+tag[0:-1]+'.txt','w', encoding='utf-8')
+    file.write(str(pool))
