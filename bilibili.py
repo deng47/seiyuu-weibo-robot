993685662a3069b6f52b6e09a4c81a76cba81f38
siqi deng <deng47@gmail.com>
2017-03-09 21:48:27 -0800

    debug

--- bilibili.py
+++ bilibili.py
@@ -6,7 +6,7 @@ import re
 def getbili(name, limit=99):
         
     dic={}
-    url='http://search.bilibili.com/ajax_api/video?keyword=' + urllib.parse.quote(name) + '&order=pubdate&page=1&_=1470859017484'
+    url='http://search.bilibili.com/ajax_api/video?keyword=' + urllib.parse.quote(name) + '&page=1&order=pubdate&tids_1=13&tids_2=152'
     req=urllib.request.Request(url)
     req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36')
     response=urllib.request.urlopen(req)
@@ -19,10 +19,8 @@ def getbili(name, limit=99):
             break
         link = tag.get('href')[4:-2]
         if link.startswith('www.bilibili') and re.search(r'[^\x00-\xff]',tag.text)!=None:
-            if link.endswith('/'):
-                dic[tag.text[10:-8]+' http://'+link] = []
-            else:
-                dic[tag.text[10:-8]+' http://'+link+'/'] = []
+            link=link[:link.find('?from')]
+            dic[tag.text[10:-8]+' http://'+link] = []
 
     return dic
 
