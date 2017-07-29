343da4cf58e9b36e23cad28ae4b19589de0ee17e
siqi deng <deng47@gmail.com>
2017-07-29 15:01:50 -0700

    update

--- seiyuu2.0/tumblr.py
+++ seiyuu2.0/tumblr.py
@@ -8,32 +8,8 @@ def gettumblr(name, limit=99):
     
     url='https://www.tumblr.com/search/' + urllib.parse.quote(name) + '/recent'
 
-    data={}
-    data['q']=name
-    data['sort']='recent'
-    data['post_view']='masonry'
-    data['blogs_before']=-1
-    data['num_blogs_shown']=0
-    data['num_posts_shown']=0
-    data['before']=0
-    data['blog_page']=2
-    data['post_page']=1
-    data['filter_nsfw']='true'
-    data['filter_post_type']='photo'
-    data['next_ad_offset']=0
-    data['ad_placement_id']=0
-    data['more_posts']='true'
-   
-    data=urllib.parse.urlencode(data).encode('utf-8')    
-    head={}
-    head['User-Agent']='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36'
-    head['referer']= url
-
-    req=urllib.request.Request(url,data,head)
-    response=urllib.request.urlopen(req)
-    html=response.read().decode('utf8')
-    html=re.sub(r'\\u[0-9a-fA-F]{4}', lambda x:eval('"' + x.group() + '"'), html)
-    soup=BeautifulSoup(html, "html.parser")
+    html=urllib.request.urlopen(url).read().decode('utf8')
+    soup = BeautifulSoup( html, "html.parser" )
     tags=soup.select('.photo')
 
     dic={}
