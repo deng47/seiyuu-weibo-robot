8743a3ca4d773066b376754c9535b6e3a535dd1b
siqi deng <deng47@gmail.com>
2017-07-31 01:20:16 -0700

    debug

--- seiyuu2.0/twitterspider.py
+++ seiyuu2.0/twitterspider.py
@@ -3,46 +3,11 @@ import json
 import urllib
 import re
 from bs4 import BeautifulSoup
-from config import USERNAME, PASSWORD
-
-def twitter_login(user, pw, user_agent):
-    """Start a requests session and login to Twitter with credentials.
-    Returned object is logged-in session."""
-    
-    tw_url = "https://twitter.com/"
-    session = requests.session()
-    first_req = session.get(tw_url)
-
-    auth_token_str = re.search(r'<input type="hidden" value="([a-zA-Z0-9]*)" name="authenticity_token"\>',
-          first_req.text)
-    authenticity_token = auth_token_str.group(1)
-
-    login_url = 'https://twitter.com/sessions'
-    
-    payload = {
-        'session[username_or_email]' : user,
-        'session[password]' : pw,
-        'remember_me' : '1',
-        'return_to_ssl' : 'true',
-        'scribe_log' : None,
-        'redirect_after_login':'/',
-        'authenticity_token': authenticity_token
-    }
-
-    login_req = session.post(login_url, data=payload, headers=user_agent)
-    print("twitter登录状态: ", login_req.status_code)
-
-    return session
-
-user_agent = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'}
-    
-session = twitter_login(USERNAME, PASSWORD, user_agent)
 
 def spider(twitter_name, limit=99):
     dic={}
     link='https://twitter.com/' + twitter_name
-    response=session.get(link)
-    html=response.text
+    html=urllib.request.urlopen(link).read()
     soup=BeautifulSoup(html, "html.parser")
 
     avatarimage=soup.select('.ProfileAvatar-image')[0].get('src')
