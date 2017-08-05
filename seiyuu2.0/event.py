96c1fb3bd8c526f28fd4f7901c3e485e2ce61068
siqi deng <deng47@gmail.com>
2017-08-04 23:23:37 -0700

    just simplify my code

--- seiyuu2.0/event.py
+++ seiyuu2.0/event.py
@@ -7,20 +7,16 @@ import json
 from bs4 import BeautifulSoup
 import re
 import datetime
-from poollog import *
 
 def getevent(name):
 
     url='https://www.eventernote.com/actors/' + urllib.parse.quote(name)
-    req=urllib.request.Request(url)
-    res=urllib.request.urlopen(req)
-    webpage=res.read().decode('utf8')
-    soup=BeautifulSoup(webpage, "html.parser")
+    html=urllib.request.urlopen(url).read()
+    soup=BeautifulSoup(html, "html.parser")
 
     content=soup.find_all('div','date')
     detail=soup.find_all('div','actor')
-    
-    #place=soup.find_all('div','place')
+    place=soup.find_all('div','place')
     
     event={}
 
@@ -28,29 +24,25 @@ def getevent(name):
         title=re.findall(r'alt="(.*)" height',str(content[index]))[0]
         pic=re.findall(r'src="(.*)" width',str(content[index]))[0]
         
-        #location=place[index].get_text().lstrip('\n ')
+        location=place[index].get_text().lstrip('\n ')
         
         date=content[index].get_text()
         eventdate=datetime.datetime.strptime(date[1:11], '%Y-%m-%d')
         today=datetime.datetime.now()
         countdown=(eventdate-today).days+1
-            
         if countdown==1:
             actor=detail[index].get_text()
-            message=(str(countdown)+' day left\n'+title+date+actor).replace('\n\n','\n')
+            message=(str(countdown)+' day left\n'+title+date+location+actor).replace('\n\n','\n')
             message=message.replace('\n\n','\n')
             event[message]=[pic]
 
         elif countdown==7 or countdown%30==1:
             actor=detail[index].get_text()
-            message=(str(countdown)+' days left\n'+title+date+actor).replace('\n\n','\n')
+            message=(str(countdown)+' days left\n'+title+date+location+actor).replace('\n\n','\n')
             message=message.replace('\n\n','\n')
             event[message]=[pic]
             
     return event
-
-
-
 
 
 
